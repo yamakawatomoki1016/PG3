d8d49db3e6fcf5acead557e761179043e5b85499
知己 山川 <tomoki10160613@gmail.com>
2023-10-23 10:33:38 +0900

    ok

--- main.cpp
+++ main.cpp
@@ -1,25 +1,26 @@
 ﻿#include <stdio.h>
+#include <iostream>
+template<typename TYPE1, typename TYPE2>
 
-template <typename Type>
-Type Min(Type a, Type b) {
-	if (static_cast<Type>(a) < static_cast<Type>(b)) {
-		return static_cast<Type>(a);
-	}
-	else {
-		return static_cast<Type>(b);
-	}
+TYPE1 Min(TYPE1 a, TYPE2 b) {
+    if (a >= b) {
+        std::cout << a << std::endl;
+        return a;
+    }
+    else {
+        std::cout << b << std::endl;
+        return static_cast<TYPE1>(b);
+    }
 }
 template<>
 char Min<char>(char a, char b) {
-	printf("数字以外は代入できません\n");
-	return 0;
+    printf("数字以外は代入できません\n");
+    return 0;
 }
-
 int main() {
-	printf("%d\n",Min<int>(114, 113));
-	printf("%f\n", Min<float>(114.0f, 113.0f));
-	printf("%lf\n", Min<double>(114, 113));
-	printf("%f\n", Min<char>(114, 113));
-
-	return 0;
+    Min<int, float>(200, 201.0f);
+    Min<char, char>(200, 201.0f);
+    Min<int, double>(200, 201.0);
+    Min<double, float>(2.0, 2.1f);
+    return 0;
 }
